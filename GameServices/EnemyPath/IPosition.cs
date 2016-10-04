fcb7463baeb93152d83ab08970bdb8408973b5be
zaug <zaug@outlook.com>
2016-10-05 00:15:39 +0200

    Komponens váz és néhány implementáció

---
+++ GameServices/EnemyPath/IPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServices
+{
+	public interface IPosition
+	{
+		void Initialize (int width, int height); // Komponens inicializáló.
+
+		void Spawn();							// Pontok létrehozása.
+
+		void Dispose (int id);					// Pontok törlése.
+
+		string GetPositions ();					// Pontok lekéréséhez.
+	}
+}
+
---
+++ GameServices/EnemyPath/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GameServices
+{
+	public class Spawner
+	{
+		private static int width, height, range;
+
+		public static void Dimensions(int w, int h) 	// Használható rajzfelület kijelölése.
+		{
+			width = w;
+			height = h;
+		}
+
+		public static void Range(int h) 	// A kezdővonal ahol a pontok létrejöhetnek.
+		{
+			range = h;
+		}
+
+		public static int spawnX()
+		{
+			Random rnd = new Random ();
+			int x = rnd.Next (20, width - 20);		// Kijelöl egy kezdőpontot a kezdővonalon.
+
+			return x;
+		}
+
+		public static int getX () {
+			return width;
+		}
+
+		public static int getY () {
+			return height;
+		}
+	}
+}
+
