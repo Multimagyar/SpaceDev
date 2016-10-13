5ef76c94b8b33657dbac8452c173c45c902fa190
zaug <zaug@outlook.com>
2016-10-13 13:21:55 +0200

    szög és távolság tulajdonságok

--- GameServices/EnemyPath/Position.cs
+++ GameServices/EnemyPath/Position.cs
@@ -2,11 +2,16 @@
 
 namespace GameServices
 {
-	public class Position 			// Objektum azonosító, x és y koordináták.
+
+	// Ez az osztály a pontok leírásához szükséges adatokat írja le.
+
+	public class Position 			
 	{
 		public int id { get; set; }
 		public int x { get; set; }
 		public int y { get; set; }
+		public int angle { get; set; }
+		public int waypoint { get; set; }
 	}
 }
 
