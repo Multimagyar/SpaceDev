5ecc0f2fb4f64ecb0e39a9a562fd63316be53c5c
zaug <zaug@outlook.com>
2016-10-17 23:10:52 +0200

    támogatott felbontások kezelése

---
+++ GameServices/EnemyPath/Resolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnemyPath
+{
+	public class Resolution
+	{
+		public void DimCheck(int w, int h) 	// Használható rajzfelület kijelölése.
+		{
+			string supported = "SupportedResolutions.txt";
+			string path = System.IO.Path.Combine (Environment.CurrentDirectory, supported);
+
+			string[] resolutions = System.IO.File.ReadAllLines (path);
+			foreach(string r in resolutions)
+			{
+				string[] resolution = r.Split ('x');
+				if((Convert.ToInt32(resolution[0]) * Convert.ToInt32(resolution[1])) == (w * h))
+				{
+					return;
+				}
+			}
+
+			throw new Exception("The provided resolution is not supported.");
+		}
+	}
+}
+
