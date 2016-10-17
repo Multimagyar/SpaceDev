2b2a62d1eece3ddebaeac781e77d94c354f126ac
zaug <zaug@outlook.com>
2016-10-17 23:13:39 +0200

    általános unit teszt megvan, még a mozgást leíró számokat kell tesztelni

---
+++ GameServices/GameServices.Test/EnemyPathTests/PathTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EnemyPath;
+using NUnit.Framework;
+
+namespace EnemyPath.Test
+{
+	[TestFixture]
+	public class PathTest
+	{
+		
+		[Test]											// Komponens teszt.
+		public void GetPath_GeneralTest_NoException()
+		{
+			Path p = new Path (800, 600);
+
+			int frames = 0;		// 10 másodperces teszt -> @60 fps -> 600 frame
+			string output = string.Empty;
+			System.IO.StreamWriter file = new System.IO.StreamWriter (Environment.CurrentDirectory + "/json.txt");
+
+			while (frames < 600)
+			{
+				file.WriteLine (p.GetPath() + '\n');
+				frames++;
+			}
+
+			Assert.AreEqual ("probe", output);
+		} 
+
+		[Test]
+		public void Motion_GeneralTest_NoException()
+		{
+			// todo...
+		}
+
+		[Test]
+		public void Dispose_ValidIdNumber_IsTrue()
+		{
+			// todo...
+		}	
+	}
+
+}
+
