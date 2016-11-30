21ac55e9aba213fc023263bad217ff55a1800704
Multimagyar <sonic7185@gmail.com>
2016-11-30 15:46:11 +0100

    [+] bugfix model edit

--- GameServices/map2loader/loader.cs
+++ GameServices/map2loader/loader.cs
@@ -2,25 +2,50 @@
 {
 	public class Materials
 	{
-		public List<Texture :: BaseTexture> textures = new List<Texture :: BaseTexture>;
+		public List<Texture.BaseTexture> textures = new List<Texture.BaseTexture>;
 		public Dictionary<string,Dictionary<string,string>> information = new Dictionary<string,Dictionary<string,string>>;
 		
-		public void texture_load(List<Texture :: BaseTexture>,Dictionary<string,string>);
+		public Materials()
+		{
+			textures = new List<Texture.BaseTexture>;
+			information = new Dictionary<string,Dictionary<string,string>>;
+		}
+		
+		public void texture_load(List<Texture.BaseTexture>,Dictionary<string,string>);
 		public void texture_delete(string);
 		public void texture_destroy();
 	}
 	public class Models
 	{
-		public List<VertexBuffer :: Resource> textures = new List<VertexBuffer :: Resource>;
+		public List<Resource.VertexBuffer> textures = new List<Resource.VertexBuffer>;
 		public Dictionary<string,Dictionary<string,string>> information = new Dictionary<string,Dictionary<string,string>>;
 		
-		public void model_load(List<Texture :: BaseTexture>,Dictionary<string,string>,VertexFormats);
+		public void model_load(List<Texture.BaseTexture>,Dictionary<string,string>,VertexFormats);
 		public void model_delete(string);
 		public void model_destroy();
 	}
-	
 }
+namespace ObjParser
+{
+	public class Obj
+	{
+		public List<Vertex> VertexList;
+		public List<Face> FaceList;
+		public List<TextureVertex> TextureList;
+		private string data[];
+		
+		public Obj()
+	    {
+            VertexList = new List<Vertex>();
+            FaceList = new List<Face>();
+			TextureList = new List<TextureVertex>();
+		}
+		public void LoadWavefrontData(string path){}
+		private void ProcessData(string line){}
+		public Resource.VertexBuffer ModelToBuffer(VertexFormats){};
 
+	}
+}
 namespace algebra
 {
 	public class vec2
@@ -56,21 +81,34 @@ namespace Map2loader
 	{
 		private int depth;
 		private string filename;
-		private Dictionary<string,string> current_class = new Dictionary<string,string>;
-		private List<algebra::vec4> planes = new List<algebra::vec4>;
-		private List<string> planes_Texture = new List<string>;
-		private List<algebra::vec4> plane_axu = new List<algebra::vec4>;
-		private List<algebra::vec4> plane_axv = new List<algebra::vec4>;
-		private List<algebra::vec3> plane_scale = new List<algebra::vec3>;
-		private List<algebra::vec3> collision_points = new List<algebra::vec3>;
+		private Dictionary<string,string> current_class;
+		private List<algebra.vec4> planes;
+		private List<string> planes_Texture;
+		private List<algebra.vec4> plane_axu;
+		private List<algebra.vec4> plane_axv;
+		private List<algebra.vec3> plane_scale;
+		private List<algebra.vec3> collision_points;
+		
+		public Datastorage()
+		{
+			int depth=0;
+			string filename="";
+			current_class = new Dictionary<string,string>;
+			planes = new List<algebra.vec4>;
+			planes_Texture = new List<string>;
+			plane_axu = new List<algebra.vec4>;
+			plane_axv = new List<algebra.vec4>;
+			plane_scale = new List<algebra.vec3>;
+			collision_points = new List<algebra.vec3>;
+		}
 		
 		public void step(){}; //ez hajtja végre a betöltés lépéseit nem valódi párhuzamosítással
 		
-		public Dictionary<string,string> readclass(current_class){}; 																	//itt olvassuk be a jelenlegi class a szótárba
-		public void ProcessPlaneData(List<algebra::vec4>,List<string>,List<algebra::vec4>,List<algebra::vec4>,List<algebra::vec3>){};	//a sík adatait olvassa
+		public Dictionary<string,string> readclass(Dictionary<string,string>){}; 																	//itt olvassuk be a jelenlegi class a szótárba
+		public void ProcessPlaneData(List<algebra.vec4>,List<string>,List<algebra.vec4>,List<algebra.vec4>,List<algebra.vec3>){};	//a sík adatait olvassa
 		public void ProcessInnerWavefront(){};						 																	//A vertex hálót olvassa
-		public List<vec4> PlaneGetIntersections(current_class){};    																	//a pontokat amelyeket három sík metszete ad 
-		public List<vec4> ShortestDistancePlane(List<vec4>){};       																	//megállapítja a legközelebbi pontot a origóhoz
+		public List<algebra.vec4> PlaneGetIntersections(List<algebra.vec4>){};    																	//a pontokat amelyeket három sík metszete ad 
+		public List<algebra.vec4> ShortestDistancePlane(List<algebra.vec4>){};       																	//megállapítja a legközelebbi pontot a origóhoz
 		public bool ObjectExists(){};                                																	//ellenörzi hogy létezik-e ilyen objektum a resource tree-ben
 		
 	}
